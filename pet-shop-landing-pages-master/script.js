ff6e1ccf46f5e04d6472624a83039531d58eba09
Your Name <you@example.com>
2023-09-18 22:04:17 +0700

    firs commit

---
+++ pet-shop-landing-pages-master/script.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".mySwiper",{
+    slidesPerView:"auto",
+    spaceBetween:30,
+    loop:true,
+    centeredSlides:true,
+    autoPlay:{
+        delay:2500,
+        disabledOnInteraction:false,
+    },
+})
