ca8a75e4317efdad7a5c4e1c1e5cd5b986fb610a
Astri Auliya <astriauliya5@gmail.com>
2023-10-07 22:22:29 +0700

    a: GET done

--- script/home.js
+++ script/home.js
@@ -1,41 +1,77 @@
 const API_URL = "https://balikpapan-1-api.up.railway.app";
 
-const fetchBlogs = async () => {
-    const BlogsContainer = document.getElementById("blog")
+const fetchProductsFeatured = async () => {
+    const ProductsContainer = document.getElementById("pro-container");
 
-    const Blog = await fetch(`${API_URL}/blog`)
-    const data = await Blog.json()
-    console.log(data)
-    //clear
+    try {
+        const response = await fetch(`${API_URL}/home/featured`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
 
-    //pake data.data karena di api nya json nya ada message, baru data
-    //jadi data ke-1 dari line 7 (await Blog.json())
-    //data ke-2 dari struktur api nya
-    data.data.forEach((element) => {
-        console.log(element)
-        const newBlogs = document.createElement('div')
-        newBlogs.classList.add()
-        newBlogs.innerHTML = `
-        <div class="blog-box">
-            <div class="blog-img">
-                <img src="${element.image}">
-            </div>
-            <div class="blog-details">
-                <h4>${element.title}</h4>
-                <p>${element.content}</p>
-                <a href="#">CONTINUE READING</a>
-            </div>
-            <h1>13/01</h1>
-        </div>`
+        data.data.forEach((element) => {
+            const newProduct = document.createElement('div');
+            newProduct.classList.add('pro');
+            newProduct.innerHTML =`
+                <img src="${element.image}" alt="">
+                <div class="des">
+                    <span>${element.brandname}</span>
+                    <h5>${element.productname}</h5>
+                    <div class="star">
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                    </div>
+                    <h4>$${element.price}</h4>
+                </div>
+                <a href="#"><i class="uil uil-shopping-cart cart"></i></a>`;
 
-        
-        BlogsContainer.appendChild(newBlogs)
-        
-    })
-
-    BlogsContainer
-
-
+            ProductsContainer.appendChild(newProduct);
+        });
+    } catch (error) {
+        console.error("Error fetching and displaying products:", error);
+    }
 };
 
-fetchBlogs()
+fetchProductsFeatured();
+
+const fetchNewArrival = async () => {
+    const ProductsContainer = document.getElementById("arrival-container");
+
+    try {
+        const response = await fetch(`${API_URL}/home/new-arrival`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
+
+        data.data.forEach((element) => {
+            const newProduct = document.createElement('div');
+            newProduct.classList.add('pro');
+            newProduct.innerHTML =`
+                <img src="${element.image}" alt="">
+                <div class="des">
+                    <span>${element.brandname}</span>
+                    <h5>${element.productname}</h5>
+                    <div class="star">
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                        <i class="uil uil-star"></i>
+                    </div>
+                    <h4>$${element.price}</h4>
+                </div>
+                <a href="#"><i class="uil uil-shopping-cart cart"></i></a>`;
+
+            ProductsContainer.appendChild(newProduct);
+        });
+    } catch (error) {
+        console.error("Error fetching and displaying products:", error);
+    }
+};
+
+fetchNewArrival();
---
+++ script/shop.js
@@ -0,0 +1,39 @@
+const API_URL = "https://balikpapan-1-api.up.railway.app";
+
+    const fetchProducts = async () => {
+        const ProductsContainer = document.getElementById("pro-container");
+
+        try {
+            const response = await fetch(`${API_URL}/shop`);
+            if (!response.ok) {
+                throw new Error("Network response was not ok.");
+            }
+            const data = await response.json();
+
+            data.data.forEach((element) => {
+                const newProduct = document.createElement('div');
+                newProduct.classList.add('pro');
+                newProduct.innerHTML =`
+                    <img src="${element.image}" alt="">
+                    <div class="des">
+                        <span>${element.brandname}</span>
+                        <h5>${element.productname}</h5>
+                        <div class="star">
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                        </div>
+                        <h4>$${element.price}</h4>
+                    </div>
+                    <a href="#"><i class="uil uil-shopping-cart cart"></i></a>`;
+
+                ProductsContainer.appendChild(newProduct);
+            });
+        } catch (error) {
+            console.error("Error fetching and displaying products:", error);
+        }
+    };
+
+    fetchProducts();
