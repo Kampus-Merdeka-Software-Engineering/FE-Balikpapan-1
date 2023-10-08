94f8d4524f97b1a40d41509c85b29dad4e3077e1
Astri Auliya <astriauliya5@gmail.com>
2023-10-08 15:10:38 +0700

    form done tinggal login, belum add to cart

--- form/form.js
+++ form/form.js
@@ -1,6 +1,6 @@
 const API_URL = "https://balikpapan-1-api.up.railway.app";
 
-
+//SIGN IN
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('signup-form');
 
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
             password: password,
         };
 
-        fetch(`${API_URL}/user`, {
+        fetch(`${API_URL}/auth/daftar`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -32,13 +32,80 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(response => {
             if (response.ok) {
-                return response.json();
+                return response.json(); // Parse the response JSON
             } else {
                 throw new Error('Registration failed');
             }
         })
         .then(data => {
-            console.log('Registration successful', data);
+            // Save name and email to localStorage
+            // localStorage.setItem('name', data.name);
+            // localStorage.setItem('email', data.email);
+        
+            Swal.fire({
+                icon: 'success',
+                title: 'Good Job!',
+                text: 'Account have been created!',
+            }).then(() => {
+                window.location.href = '/';
+            });
+        })
+        .catch(error => {
+            console.error('Registration failed', error);
+        });
+    });
+});
+
+
+//LOGIN
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('signup-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = form.querySelector('input[name="name"]').value;
+        const email = form.querySelector('input[name="email"]').value;
+        const password = form.querySelector('input[name="password"]').value;
+        const confirmPassword = form.querySelector('input[name="confirmPassword"]').value;
+
+        if (password !== confirmPassword) {
+            alert('Password and confirm password do not match');
+            return;
+        }
+
+        const userData = {
+            name: name,
+            email: email,
+            password: password,
+        };
+
+        fetch(`${API_URL}/auth/daftar`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json(); // Parse the response JSON
+            } else {
+                throw new Error('Registration failed');
+            }
+        })
+        .then(data => {
+            // Save name and email to localStorage
+            // localStorage.setItem('name', data.name);
+            // localStorage.setItem('email', data.email);
+        
+            Swal.fire({
+                icon: 'success',
+                title: 'Good Job!',
+                text: 'Account have been created!',
+            }).then(() => {
+                window.location.href = '/';
+            });
         })
         .catch(error => {
             console.error('Registration failed', error);
--- script/sproduct.js
+++ script/sproduct.js
@@ -1,32 +1,70 @@
 const API_URL = "https://balikpapan-1-api.up.railway.app";
 
-        const urlParams = new URLSearchParams(window.location.search);
-        const itemId = urlParams.get('id');
-        const titleProduct = document.getElementById("title-product");
-        const priceElement = document.getElementById("price");
-        const imageProduct = document.getElementById("image-product");
-        const productDescription = document.getElementById("product-description");
-
-        // Fetch detailed data based on the ID and display it
-        const fetchDetails = async () => {
-            try {
-                const response = await fetch(`${API_URL}/shop/${itemId}`);
-                if (!response.ok) {
-                    throw new Error("Network response was not ok.");
-                }
-                const data = await response.json();
-                console.log(data);
-
-                // Update the elements with data from the API
-                titleProduct.textContent = data.data.productname;
-                priceElement.textContent = `$ ${data.data.price}`;
-                imageProduct.src = data.data.image;
+const urlParams = new URLSearchParams(window.location.search);
+const itemId = urlParams.get('id');
+const titleProduct = document.getElementById("title-product");
+const priceElement = document.getElementById("price");
+const imageProduct = document.getElementById("image-product");
+const productDescription = document.getElementById("product-description");
+const detailsAPI = `${API_URL}/shop/${itemId}`;
 
 
+// Fetch detailed data based on the ID and display it
+const fetchDetails = async () => {
+    try {
+        const response = await fetch(detailsAPI);
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
+        console.log(data);
 
-            } catch (error) {
-                console.error("Error fetching details:", error);
-            }
+        // Update the elements with data from the API
+        titleProduct.textContent = data.data.productname;
+        priceElement.textContent = `$ ${data.data.price}`;
+        imageProduct.src = data.data.image;
+
+
+
+    } catch (error) {
+        console.error("Error fetching details:", error);
+    }
+};
+
+fetchDetails();
+
+
+//ADD TO CART
+const addToCartButton = document.getElementById("add-to-cart-button");
+
+addToCartButton.addEventListener("click", () => {
+    const selectedQuantity = parseInt(document.querySelector("input[type='number']").value, 10);
+
+    if (selectedQuantity > 0) {
+        const cartData = {
+            productId: itemId,
+            quantity: selectedQuantity
         };
 
-        fetchDetails();
+        //POST request
+        fetch(`${detailsAPI}/cart`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(cartData)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to add the product to the cart.");
+            }
+            console.log("Product added to the cart successfully!");
+        })
+        .catch(error => {
+            console.error("Error adding the product to the cart:", error);
+        });
+    } else {
+        // if user not select valid quantity
+        console.error("Please select a valid quantity before adding to the cart.");
+    }
+});
